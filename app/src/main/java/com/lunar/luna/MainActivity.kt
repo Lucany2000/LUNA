dc96230885622d0e15fb8a6f0093fbb9bc47cada
Luca Celentano <LucaNY2000@gmail.com>
2023-04-21 17:43:58 -0400

    gh-1 textView, buttons, and Toast

--- app/src/main/java/com/lunar/luna/MainActivity.kt
+++ app/src/main/java/com/lunar/luna/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lunar.luna
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnClickMe = findViewById<Button>(R.id.mybutton)
+        val changeText = findViewById<TextView>(R.id.textView)
+        btnClickMe.text = "Click me"
+        changeText.text = "0"
+        var timesClicked = 0
+        btnClickMe.setOnClickListener{
+            //btnClickMe.text = "Clicked Me"
+            timesClicked += 1
+            changeText.text = timesClicked.toString() //"Hello User"
+            Toast.makeText(this, "Hey, I'm toast", Toast.LENGTH_LONG).show()
+        }
     }
 }
