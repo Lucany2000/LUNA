1ecf91795a91a84014416733e0ce839a294be702
Lucany2000 <LucaNY2000@gmail.com>
2023-12-23 17:30:16 -0500

    gh-20 error message and manual redirect to app settings

--- app/src/main/java/com/luna/main/MainActivity.kt
+++ app/src/main/java/com/luna/main/MainActivity.kt
@@ -9,6 +9,7 @@ import android.content.ContentResolver
 import android.content.ContentUris
 import android.net.Uri
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
 import android.os.Bundle
@@ -23,27 +24,74 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import android.database.MergeCursor
 import android.provider.Settings
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSION_CODE = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
 
-        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) {
-            ActivityCompat.requestPermissions(
+        if (ContextCompat.checkSelfPermission(
                 this,
-                arrayOf(Manifest.permission.READ_MEDIA_AUDIO),
-                0
-            )
+                Manifest.permission.READ_MEDIA_AUDIO
+            ) != PackageManager.PERMISSION_GRANTED ||
+
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            // Permission is not granted, request it
+            if (Build.VERSION.SDK_INT > Build.VERSION_CODES.S_V2) {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.READ_MEDIA_AUDIO),
+                        REQUEST_PERMISSION_CODE
+                    )
+            } else {
+                    ActivityCompat.requestPermissions(
+                        this,
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        REQUEST_PERMISSION_CODE
+                    )
+            }
         } else {
-            ActivityCompat.requestPermissions(
-                this,
-                arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
-                0
-            )
+            main()
+
         }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == REQUEST_PERMISSION_CODE) {
+            // Check if permissions are granted
+            if (grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED
+            ) {
+                main()
+            } else {
+               errorMsg("No Audio Found")
+            }
+        }
+    }
+
+    private fun main() {
+        val audioFiles = getAllAudioFiles(this)
+        val textToShow = audioFiles.joinToString("\n")
+        displayText(textToShow)
+    }
+
+    private fun displayText(text: String) {
 
         val rootLayout = LinearLayout(this)
         rootLayout.layoutParams = LinearLayout.LayoutParams(
@@ -51,6 +99,7 @@ class MainActivity : AppCompatActivity() {
             LinearLayout.LayoutParams.MATCH_PARENT
         )
         rootLayout.orientation = LinearLayout.VERTICAL
+        rootLayout.gravity = Gravity.CENTER
         // Set a solid color background (you can use Color.parseColor for hex colors)
 
         val textView = TextView(this)
@@ -59,35 +108,54 @@ class MainActivity : AppCompatActivity() {
             LinearLayout.LayoutParams.WRAP_CONTENT
         )
 
-//        if (ContextCompat.checkSelfPermission(
-//                this,
-//                Manifest.permission.READ_MEDIA_AUDIO
-//            ) != PackageManager.PERMISSION_GRANTED
-//        ) {
-//            // Permission is not granted, request it
-//            ActivityCompat.requestPermissions(
-//                this,
-//                arrayOf(Manifest.permission.READ_MEDIA_AUDIO),
-//                0
-//            )
-//        }
-
-        val audioFiles = getAllAudioFiles(this)
-        val textToShow = audioFiles.joinToString("\n")
-        Log.d("MainActivity", "$audioFiles")
-
-        textView.text = textToShow
+        textView.text = text
         textView.gravity = Gravity.CENTER
         textView.setTextColor(Color.BLACK)
 
-//        textView.setTextSize(24f)
+        textView.setTextSize(24f)
 
         rootLayout.addView(textView)
-
         setContentView(rootLayout)
-
-
     }
+
+    fun errorMsg(text: String) {
+        val rootLayout = LinearLayout(this)
+        rootLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.MATCH_PARENT
+        )
+        rootLayout.orientation = LinearLayout.VERTICAL
+        rootLayout.gravity = Gravity.CENTER
+        // Set a solid color background (you can use Color.parseColor for hex colors)
+
+        val textView = TextView(this)
+        textView.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+
+        textView.text = text
+        textView.gravity = Gravity.CENTER
+        textView.setTextColor(Color.BLACK)
+
+        textView.append("\n\nTo enable the permission, go to app settings.")
+        textView.setOnClickListener {
+            openAppSettings()
+        }
+
+        textView.setTextSize(24f)
+
+        rootLayout.addView(textView)
+        setContentView(rootLayout)
+    }
+
+    private fun openAppSettings() {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri: Uri = Uri.fromParts("package", packageName, null)
+        intent.data = uri
+        startActivity(intent)
+    }
+
 
     private fun getAllAudioFiles(context: Context): List<Audio> {
 
@@ -97,16 +165,15 @@ class MainActivity : AppCompatActivity() {
             MediaStore.Audio.Media._ID,
             MediaStore.Audio.Media.TITLE,
             MediaStore.Audio.Media.DISPLAY_NAME,
-//            MediaStore.Audio.Media.ARTIST,
-//            MediaStore.Audio.Media.ARTIST_ID,
-//            MediaStore.Audio.Media.ALBUM,
-//            MediaStore.Audio.Media.ALBUM_ID,
-//            MediaStore.Audio.Media.ALBUM_ARTIST,
-//            MediaStore.Audio.Media.DEFAULT_SORT_ORDER,
-//            MediaStore.Audio.Media.TRACK,
-//            MediaStore.Audio.Media.MIME_TYPE,
-//            MediaStore.Audio.Media.IS_DOWNLOAD,
-//            MediaStore.Audio.Media.DATA
+            MediaStore.Audio.Media.ARTIST,
+            MediaStore.Audio.Media.ARTIST_ID,
+            MediaStore.Audio.Media.ALBUM,
+            MediaStore.Audio.Media.ALBUM_ID,
+            MediaStore.Audio.Media.ALBUM_ARTIST,
+            MediaStore.Audio.Media.TRACK,
+            MediaStore.Audio.Media.MIME_TYPE,
+            MediaStore.Audio.Media.IS_DOWNLOAD,
+            MediaStore.Audio.Media.DATA
         )
 
         val selection = (
@@ -165,41 +232,39 @@ class MainActivity : AppCompatActivity() {
             val idColumn = it.getColumnIndex(MediaStore.Audio.Media._ID)
             val nameColumn = it.getColumnIndex(MediaStore.Audio.Media.DISPLAY_NAME)
             val titleColumn = it.getColumnIndex(MediaStore.Audio.Media.TITLE)
-//            val artistColumn = it.getColumnIndex(MediaStore.Audio.Media.ARTIST)
-//            val artistIdColumn = it.getColumnIndex(MediaStore.Audio.Media.ARTIST_ID)
-//            val albumColumn = it.getColumnIndex(MediaStore.Audio.Media.ALBUM)
-//            val albumIdColumn = it.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)
-//            val albumartistColumn = it.getColumnIndex(MediaStore.Audio.Media.ALBUM_ARTIST)
-//            val defsortColumn = it.getColumnIndex(MediaStore.Audio.Media.DEFAULT_SORT_ORDER)
-//            val trackColumn = it.getColumnIndex(MediaStore.Audio.Media.TRACK)
-//            val mimeColumn = it.getColumnIndex(MediaStore.Audio.Media.MIME_TYPE)
-//            val isDownloadColumn = it.getColumnIndex(MediaStore.Audio.Media.IS_DOWNLOAD)
-//            val dataColumn = it.getColumnIndex(MediaStore.Audio.Media.DATA)
+            val artistColumn = it.getColumnIndex(MediaStore.Audio.Media.ARTIST)
+            val artistIdColumn = it.getColumnIndex(MediaStore.Audio.Media.ARTIST_ID)
+            val albumColumn = it.getColumnIndex(MediaStore.Audio.Media.ALBUM)
+            val albumIdColumn = it.getColumnIndex(MediaStore.Audio.Media.ALBUM_ID)
+            val albumartistColumn = it.getColumnIndex(MediaStore.Audio.Media.ALBUM_ARTIST)
+            val trackColumn = it.getColumnIndex(MediaStore.Audio.Media.TRACK)
+            val mimeColumn = it.getColumnIndex(MediaStore.Audio.Media.MIME_TYPE)
+            val isDownloadColumn = it.getColumnIndex(MediaStore.Audio.Media.IS_DOWNLOAD)
+            val dataColumn = it.getColumnIndex(MediaStore.Audio.Media.DATA)
 
             while (it.moveToNext()) {
                 val id = it.getLong(idColumn)
                 val name = it.getString(nameColumn)
                 val title = it.getString(titleColumn)
-//                val artist = it.getString(artistColumn)
-//                val artistId = it.getLong(artistIdColumn)
-//                val album = it.getString(albumColumn)
-//                val albumId = it.getLong(albumIdColumn)
-//                val albumartist = it.getString(albumartistColumn)
-//                val defsort = it.getLong(defsortColumn)
-//                val track = it.getLong(trackColumn)
-//                val mime = it.getString(mimeColumn)
-//                val isDownload = it.getLong(isDownloadColumn)
-//                val data = it.getString(dataColumn)
+                val artist = it.getString(artistColumn)
+                val artistId = it.getLong(artistIdColumn)
+                val album = it.getString(albumColumn)
+                val albumId = it.getLong(albumIdColumn)
+                val albumartist = it.getString(albumartistColumn) ?: "Unknown"
+                val track = it.getLong(trackColumn)
+                val mime = it.getString(mimeColumn)
+                val isDownload = it.getLong(isDownloadColumn)
+                val data = it.getString(dataColumn)
 
-//                val uri = ContentUris.withAppendedId(
-//                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
-//                    id
-//                )
+                val uri = ContentUris.withAppendedId(
+                    MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    id
+                )
                 audio.add(
                     Audio(
                         id, name, title,
-//                        artist, artistId, album, albumId, albumartist, defsort,
-//                        track, mime, isDownload, data
+                        artist, artistId, album, albumId, albumartist,
+                        track, mime, isDownload, data, uri
                     )
                 )
             }
@@ -211,15 +276,15 @@ class MainActivity : AppCompatActivity() {
         val id: Long,
         val name: String,
         val title: String,
-//        val artist: String,
-//        val artistId: Long,
-//        val album: String,
-//        val albumId: Long,
-//        val albumartist: String,
-//        val defsort: Long,
-//        val track: Long,
-//        val mime: String,
-//        val isDownload: Long,
-//        val data: String,
+        val artist: String,
+        val artistId: Long,
+        val album: String,
+        val albumId: Long,
+        val albumartist: String,
+        val track: Long,
+        val mime: String,
+        val isDownload: Long,
+        val data: String,
+        val uri: Uri
     )
 }
